d6b6994d84d7ddede0f55d5ce7c9e501b7b24c40
Adrian Camacho <acr77frb@gmail.com>
2024-12-11 01:07:48 -0600

    Se editó la estructura HTML; se agregaron propiedades CSS responsivas

---
+++ app/main.js
@@ -0,0 +1,8 @@
+let h1 = document.getElementsByTagName("h1")[0];
+
+// h1.addEventListener("click", e => {
+//   e.preventDefault();
+//   console.log("Henlo");
+//   e.target.style.color = "red";
+//   e.target.style.transition = "300ms";
+// })
