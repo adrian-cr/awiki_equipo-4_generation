89692d91bbd14375bcebde554f592dcf406ffd4a
Adrian Camacho <acr77frb@gmail.com>
2024-12-28 14:40:49 -0600

    Se eliminaron archivos JS duplicados

--- app/main.js
+++
@@ -1,7 +0,0 @@
-let h1 = document.getElementsByTagName("h1")[0];
-
-h1.addEventListener("click", e => {
-   e.preventDefault();
-   e.target.style.color = "#77aaff";
-   e.target.style.transition = "500ms";
-})
