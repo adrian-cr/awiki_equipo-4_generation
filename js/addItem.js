41af565c17cbd413c8e28084a871f083f8b06b9c
Diana Isabel Cantillo <cantillo.diana.isabel@gmail.com>
2025-01-14 20:56:58 -0600

    Se modificaron los archivos de json, additem.js, resultados busqueda de html y css

--- js/addItem.js
+++ js/addItem.js
@@ -4,8 +4,11 @@ function addItem(item){
       '    <img src="' + item.imagen + '" class="card-img-top" alt="image">\n' +
       '    <div class="card-body">\n' +
       '        <h5 class="card-title">'+ item.nombreNegocio +'</h5>\n' +
-      '        <p class="card-text">'+item.descripcion+'</p>\n' +
-      '        <a href="#" class="btn btn-primary">Add</a>\n' +
+      '        <p class="card-categoria">' + 'Categoría: ' + item.categoria + '</p>\n' +
+      '        <p class="card-Description">'+ item.descripcion +'</p>\n' +
+      '        <p class="card-direction">' + 'Ubicación: ' + item.ubicacion.municipio +', ' + item.ubicacion.estado + '</p>\n' +
+      '        <p class="card-rating">' + '<strong>Ranking: </strong>' + item.rating + '<p/>\n' +
+      '        <a href="#" id = "verMas" class="btn btn-primary" <style> </style>Ver más</a>\n' +
       '    </div>\n' +
       '</div>\n' +
       '<br/>';
@@ -14,12 +17,9 @@ function addItem(item){
 }
 
 fetch("../../data/listings.json")
-  .then(
-    res => {
-      res.json()
-    })
-  .then(
-    res => {
-      listings = listings.concat(res.data);
-      listings.forEach(e => addItem(e));
+  .then( 
+    res => res.json())
+  .then( res => {
+    listings = listings.concat(res.data);
+    listings.forEach(e => addItem(e));      
     });
