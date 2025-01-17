6d3a76218a95b39f3a07badb63feee533af00e88
jorg3mendoza <jorgemendozaaviles@hotmail.com>
2025-01-16 23:13:09 -0600

    funcionalidad cloudinary

--- js/basesDatos/publicacionUsuario.js
+++ js/basesDatos/publicacionUsuario.js
@@ -1,12 +1,36 @@
+'use strict';
+let url_imagenes = [];
+const boton_imagen = document.querySelector('#btn-imagen');
+const imagen = document.querySelector('#user-photo');
+
+let widget_cloudinary = cloudinary.createUploadWidget({
+    cloudName: 'dawddkonk',
+    uploadPreset: 'preset_awiki'
+}, (err, result) => {
+    if(!err && result && result.event === 'success'){
+        console.log('Imagen subida con exito', result.info);
+        imagen.src = result.info.secure_url;
+        url_imagenes.push(result.info.secure_url);
+    }
+});
+
+boton_imagen.addEventListener('click', () => {
+    widget_cloudinary.open();
+}, false);
+
+
+
 const formulario = document.getElementById("fullForm");
 
 const procesaTodo = (event) =>{
     event.preventDefault();
     const datos = new FormData(event.target);
     const datosCompletos = Object.fromEntries(datos.entries());
-    console.log(JSON.stringify(datosCompletos));
+    datosCompletos.imagenes = url_imagenes;
+    console.log('JSON Final con imágenes:', JSON.stringify(datosCompletos)); // Verificar el JSON
     return datosCompletos;
-}
+
+    }
 
 const postData = async(newPublicacion) => {
     try{
