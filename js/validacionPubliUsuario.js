b126ae62ff155e63a9dce308927dfbd5a4be8284
Diana Isabel Cantillo <cantillo.diana.isabel@gmail.com>
2025-01-21 23:08:03 -0600

    se aceptan cambios

--- js/validacionPubliUsuario.js
+++ js/validacionPubliUsuario.js
@@ -1,28 +1,16 @@
 const txtpublication = document.getElementById("validationTextarea");
 const mensajeError = document.getElementById("mensaje-error");
 const btnPublicar = document.getElementById("btnPublicar");
-<<<<<<< HEAD
 import * as validators from "../modules/validators.js";
 
 function validarPublication() {
     if (!validators.isPostValid(txtpublication.id)) {
-=======
-
-
-function validarPublication() {
-    if (txtpublication.value.length < 8) {
->>>>>>> Rama_VD
       mensajeError.style.display = "block";
       mensajeError.insertAdjacentHTML("beforeend", `<li>Ingrese una publicación válida.</li>`);
       return false;
     }
-<<<<<<< HEAD
      return true;
    }
-=======
-    return true;
-  }
->>>>>>> Rama_VD
 
   function borrarErrores(){
     mensajeError.innerHTML = "";
