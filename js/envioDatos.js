7c61c90a484d294f71535a2373544536bf04c2eb
Diana Isabel Cantillo <cantillo.diana.isabel@gmail.com>
2025-01-21 22:51:50 -0600

    se aceptan cambios

--- js/envioDatos.js
+++ js/envioDatos.js
@@ -11,24 +11,16 @@ window.onload = function() {
       // these IDs from the previous steps
       emailjs.sendForm('service_eikj0hg', 'template_wi3e0gk', this)
           .then(() => {
-<<<<<<< HEAD
             Swal.fire({
               title: "¡Datos enviados con éxito!",
               text: "Gracias por contactarnos, te responderemos pronto",
               icon: "success",
               draggable: true
             });
-=======
-              console.log('SUCCESS!');
->>>>>>> Rama_VD
           }, (error) => {
               console.log('FAILED...', error);
           });
 
-<<<<<<< HEAD
     // window.location.assign("../index.html")
-=======
-    window.location.assign("../index.html")
->>>>>>> Rama_VD
   });
 }
