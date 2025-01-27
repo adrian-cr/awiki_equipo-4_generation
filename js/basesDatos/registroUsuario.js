72ec12cb0bee0a804f959c0bc6346065abfec57a
Diana Isabel Cantillo <cantillo.diana.isabel@gmail.com>
2025-01-27 17:57:02 -0600

    Se agrega inicio de sesión

--- js/basesDatos/registroUsuario.js
+++ js/basesDatos/registroUsuario.js
@@ -1,30 +1,15 @@
-// const formulario = document.getElementById("formulario2");
+const loginuser = document.getElementById("formulario1");
+loginuser.addEventListener("submit", (e) =>{
+    e.preventDefault();
 
-// // const arreglo=[];
-
-
-// const procesaTodo = (event) =>{
-//     event.preventDefault();
-//     const datos = new FormData(event.target);
-//     const datosCompletos = Object.fromEntries(datos.entries());
-//     console.log(datosCompletos)
-//     console.log(JSON.stringify(datosCompletos)); // Verificar el JSON
-//     //Obtener los usuarios previos desde localStorage (si existen) y se agregan
-//     const usuariosPrevios=JSON.parse(localStorage.getItem('usuarios'))|| [];
-//     usuariosPrevios.push(datosCompletos);
-
-//     // arreglo.push(datosCompletos);
-//     // console.log(datosCompletos);
-
-//     //Guarda el nuevo arreglo de usuarios
-//     localStorage.setItem('usuarios', JSON.stringify(usuariosPrevios));
-
-//     return datosCompletos;
-
-// }
-
-// formulario.addEventListener("submit", (event) => {
-//     const newUsuario = procesaTodo(event);
-// //     localStorage.setItem('usuario', JSON.stringify(newUsuario));
-// //     postData(newPublicacion);
-// })
+    const email = document.getElementById("correoSesion").value;
+    const contraseña = document.getElementById("contraseñaSesion").value;
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
+    const validUser = usuarios.find(Usuario => Usuario.campoCorreo === email && Usuario.campoContraseña === contraseña)
+    if(!validUser){
+        return alert ("Usuario y/o contraseña incorrectos!")
+    }else {
+        alert(`Bienvenida ${validUser.campoCorreo}`)
+    }
+}
+)
