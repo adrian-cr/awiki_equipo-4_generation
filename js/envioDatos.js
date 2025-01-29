fc16529159f24e44c8a6e4ebbaa82a3db8bc48ee
Liz Delgado <lizdelgadonav26@gmail.com>
2025-01-29 00:00:08 -0600

    Actualización de la sweet alert del formulario de contacto

--- js/envioDatos.js
+++ js/envioDatos.js
@@ -1,37 +1,53 @@
-const formularioContacto = document.getElementById("formulario-contacto");
+
 (function() {
+ 
+  const txtNombre = document.getElementById("nombre");
+  const txtTelefono = document.getElementById("telefono");
+  const txtEmail = document.getElementById("email");
+  const txtMensaje = document.getElementById("mensaje");
+  const formularioContacto = document.getElementById("formulario-contacto");
+
   emailjs.init({
     publicKey: "yMOZxXqsrg5p2k8rS",
   });
+
+  function limpiarCampos() {
+    txtNombre.value = "";
+    txtTelefono.value = "";
+    txtEmail.value = "";
+    txtMensaje.value = "";
+  }
+
+  window.onload = function() {
+    formularioContacto.addEventListener('submit', function(event) {
+      event.preventDefault();
+      if (!window.validarDatos()) { 
+        return; 
+      }
+      emailjs.sendForm('service_eikj0hg', 'template_wi3e0gk', this)
+        .then(() => {
+          Swal.fire({
+            title: "¡Datos enviados con éxito!",
+            text: "Gracias por contactarnos, te responderemos pronto",
+            icon: "success",
+            customClass: {
+              title: 'swal-title',
+              text: 'swal-text',
+              popup: 'swal-popup',
+              confirmButton: 'swal-confirm-button',
+              icon: 'custom-icon'
+            },
+             confirmButtonText: 'Salir',
+            buttonsStyling: false
+          }).then(() => {
+            limpiarCampos(); 
+          });
+        }, (error) => {
+          console.log('FAILED...', error);
+        });
+    });
+  }
 })();
 
-window.onload = function() {
-  formularioContacto.addEventListener('submit', function(event) {
-      event.preventDefault();
-      // these IDs from the previous steps
-      emailjs.sendForm('service_eikj0hg', 'template_wi3e0gk', this)
-          .then(() => {
-            Swal.fire({
-              title: "¡Datos enviados con éxito!",
-              text: "Gracias por contactarnos, te responderemos pronto",
-              icon: "success",
-              customClass: {
-                title: 'swal-title',
-                text: 'swal-text',
-                popup: 'swal-popup',
-                confirmButton: 'swal-confirm-button'
-              },
-              buttonsStyling: false
-            });
-          }, (error) => {
-            console.log('FAILED...', error);
-          });
-          //     draggable: true
-          //   });
-          // }, (error) => {
-          //     console.log('FAILED...', error);
-          // });
 
-    // window.location.assign("../index.html")
-  });
-}
+
