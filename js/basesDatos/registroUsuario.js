c48ff409d4d52034cba159550925a780d6ff43e6
Liz Delgado <lizdelgadonav26@gmail.com>
2025-01-29 01:17:32 -0600

    Validiación y actualización de la sweet alert del inicio de sesión y redirigir al index

--- js/basesDatos/registroUsuario.js
+++ js/basesDatos/registroUsuario.js
@@ -1,15 +1,47 @@
-const loginuser = document.getElementById("formulario1");
-loginuser.addEventListener("submit", (e) =>{
-    e.preventDefault();
 
-    const email = document.getElementById("correoSesion").value;
-    const contraseña = document.getElementById("contraseñaSesion").value;
-    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
-    const validUser = usuarios.find(Usuario => Usuario.campoCorreo === email && Usuario.campoContraseña === contraseña)
-    if(!validUser){
-        //return alert ("Usuario y/o contraseña incorrectos!")
-    }else {
-        alert(`Bienvenida/o ${validUser.campoCorreo}`)
-    }
-}
-)
+const loginuser = document.getElementById("formulario1");
+loginuser.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("correoSesion").value;
+  const contraseña = document.getElementById("contraseñaSesion").value;
+  const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  const validUser = usuarios.find(Usuario => Usuario.campoCorreo === email && Usuario.campoContraseña === contraseña);
+
+  if (!validUser) {
+    Swal.fire({
+      title: "Error",
+      text: "Usuario y/o contraseña incorrectos!",
+      icon: "error",
+      confirmButtonText: "Entendido",
+      buttonsStyling: false,
+      customClass: {
+        title: 'swal-title',
+        text: 'swal-text',
+        popup: 'swal-popup',
+        confirmButton: 'swal-confirm-button',
+        icon: 'custom-icon'
+      },
+      
+    });
+  } else {
+    Swal.fire({
+      title: "¡Bienvenid@!",
+      text: `Bienvenid@ ${validUser.campoCorreo}, ahora eres parte de Awiki :)`,
+      icon: "success",
+      confirmButtonText: "Continuar",
+      buttonsStyling: false,
+      customClass: {
+        title: 'swal-title',
+        text: 'swal-text',
+        popup: 'swal-popup',
+        confirmButton: 'swal-confirm-button',
+        icon: 'custom-icon'
+      },
+
+      willClose: () => {
+        window.location.href = "/index.html"; 
+      }
+    });
+  }
+});
