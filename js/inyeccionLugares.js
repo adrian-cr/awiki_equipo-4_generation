98ce7d87b71933debe149b90b9f9e3635c273a54
sggt <sofiaggt2305@gmail.com>
2025-02-10 18:39:20 -0600

    Se agrega borrador de Página Perfil de empresa

--- js/inyeccionLugares.js
+++ js/inyeccionLugares.js
@@ -1,43 +1,38 @@
-const cardBody=document.getElementById("descripcionLugares");
-const mapa = document.getElementById("map");
-const carruselEmpresa= document.getElementById("imagen-c");
-const URLMain="https://fakestoreapi.com/products/2"
+const urlParams= new URLSearchParams(window.location.search);
+const productoId= urlParams.get('product');
+console.log("ID del producto del URL", productoId);
+const titulo=document.getElementById("nombreLugar");
+const descripcion= document.getElementById("descripcionLugares");
+const imagen= document.getElementById("imagenLugar");
+const URL='https://fakestoreapi.com/products/';
 
-function getData(){
-    fetch(URLMain).then((response) => {
-        console.log(response);
-        response.json().then((res)=>{
-            cardBody.innerHTML = `Titutlo: ${res.title}
-            </br>
-            Precio: ${res.price}
-            `;
-        });
-    }).catch((err) => {
-        alertError.innerText=`Problema al traer la información ${err}`;
-        alertError.style.display="block";
+console.log(URL);
+
+fetch(URL)
+.then((response)=>{
+    console.log(response);
+    response.json().then((res)=>{
+        const producto= res.find(product=> product.id==productoId)
+
+        if(producto){
+            titulo.textContent=producto.title;
+            descripcion.textContent=producto.description;
+            imagen.src="";
+        }else{
+            titulo.textContent="No hay datos";
+            descripcion.textContent="No hay información que mostrar";
+            imagen.src="";
+        }
     })
-}//getData
-
-// function createCards(res){
-//     res.forEach((res)=> {
-//       divProductos.insertAdjacentHTML("beforeend", `
-//         <div class="card border-dark mb-3" style="max-width: 540px;">
-//     <div class="row g-0">
-//       <div class="col-md-4">
-//         <img src="${res.image}" class="img-fluid rounded-start" alt="...">
-//       </div>
-//       <div class="col-md-8">
-//         <div class="card-body">
-//           <h5 class="card-title">${res.title}</h5>
-//           <p class="card-text">${res.description}</p>
-//           <p class="card-text"><small class="text-body-secondary"> $ ${res.price} USD</small></p>
-//           <p class="card-tetx"><small class="text-body-category"> Category: ${res.category}</small></p>
-//           </div>
-//       </div>
-//     </div>
-//   </div>`)
-//     });
-//   }
+})
+.catch(err=>{
+    console.log("No se pudieron obtener los productos");
+    titulo.textContent="Error al cargar la información";
+    descripcion.textContent="Lo sentimos, no hay información que mostrar";
+})
 
 
-getData();
+
+
+
+
