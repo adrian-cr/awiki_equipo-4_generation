b7eec7d891bcaf4d41a51c6fdf8ff3a13bf191ac
Diana Isabel Cantillo <cantillo.diana.isabel@gmail.com>
2025-02-13 20:47:10 -0600

    solución a problemas de ruta

--- js/pages/resultadosBusqueda/addListingCards.js
+++ js/pages/resultadosBusqueda/addListingCards.js
@@ -60,7 +60,7 @@ function addListingCard(listing){
   listingsContainer.insertAdjacentHTML("beforeend", listingCardHTML);
 }
 
-fetch("../../../data/newListings.json")
+fetch("../../data/listings.json")
   .then(
     res => res.json())
   .then( res => {
