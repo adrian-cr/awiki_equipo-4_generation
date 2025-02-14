a2b319314fd08836fb8455296d495c8cc55333dc
Adrian Camacho <acr77frb@gmail.com>
2025-02-13 23:18:41 -0600

    Se modificaron y añadieron funcionalidades a módulos Formatters y Utils

--- modules/utils.js
+++ modules/utils.js
@@ -1,3 +1,18 @@
 export const capitalize = str => {
   return str[0].toUpperCase() + str.slice(1, str.length);
 }
+
+export const fillStar = (position, rating) => {
+  //rating=4.5, starPosition=3 --> STAR FILLED
+  rating = parseFloat(rating);
+  position = parseFloat(position);
+  if (rating >= position) {
+    return "-fill";
+  }
+  //rating=1.5, starPosition=3 --> STAR EMPTY
+  if (rating > position - 1) {
+    return "-half"
+  }
+  return "";
+
+}
